88701794e82a66cb7b2416e0a7e3cb8899696b07
seantempleton-lang <sean.templeton@gmail.com>
2026-04-20 11:36:28 +1200

    major UI refresh and branding

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,45 +8,31 @@ export default function Layout({ me, children }) {
     <>
       <div className="topbar">
         <div className="topbar-inner">
-          <div className="brand">McMillan LMS</div>
+          <div className="brand">
+            <span className="brand-mcmillan">M<sup style={{fontSize:'0.6em'}}>c</sup>MILLAN</span>
+            <span className="brand-drilling">Drilling</span>
+            <span className="brand-lms">LMS</span>
+          </div>
           <div className="nav">
-            <NavLink
-              className={({ isActive }) => `chip ${isActive ? 'active' : ''}`}
-              to="/"
-            >
+            <NavLink className={({ isActive }) => `chip ${isActive ? 'active' : ''}`} to="/">
               Dashboard
             </NavLink>
-
             {(me?.role === 'SUPERVISOR' || me?.role === 'ADMIN') && (<>
-              <NavLink
-                className={({ isActive }) => `chip ${isActive ? 'active' : ''}`}
-                to="/supervisor/sessions"
-              >
+              <NavLink className={({ isActive }) => `chip ${isActive ? 'active' : ''}`} to="/supervisor/sessions">
                 Sessions
               </NavLink>
-              <NavLink
-                className={({ isActive }) => `chip ${isActive ? 'active' : ''}`}
-                to="/supervisor/competencies"
-              >
+              <NavLink className={({ isActive }) => `chip ${isActive ? 'active' : ''}`} to="/supervisor/competencies">
                 Competencies
               </NavLink>
-              <NavLink
-                className={({ isActive }) => `chip ${isActive ? 'active' : ''}`}
-                to="/supervisor/matrix"
-              >
+              <NavLink className={({ isActive }) => `chip ${isActive ? 'active' : ''}`} to="/supervisor/matrix">
                 Matrix
               </NavLink>
             </>)}
-
             {me?.role === 'ADMIN' && (
-              <NavLink
-                className={({ isActive }) => `chip ${isActive ? 'active' : ''}`}
-                to="/admin/users"
-              >
+              <NavLink className={({ isActive }) => `chip ${isActive ? 'active' : ''}`} to="/admin/users">
                 Users
               </NavLink>
             )}
-
             <button className="chip" onClick={logout}>Logout</button>
           </div>
         </div>
